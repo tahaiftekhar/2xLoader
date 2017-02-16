8c0f0d61e07fb306bbc74c1e2833cc6b38d02442
Taha Iftekhar <taha_iftekhar@yahoo.com>
2017-02-16 12:22:12 +0500

    Initial Commit Lots of high quality images that will load slowly, with a custom JS file called '2xLoader.js' which is the key file. 'with.html' shows how to implement it

---
+++ js/2xLoader.js
@@ -0,0 +1,20 @@
+var current, obj, current_index = 0;
+
+function onexload(){
+	obj = $('[data-load]');
+	load_next();
+}
+
+function load_next(){
+	current = obj.eq(current_index);
+	current_index = current_index + 1;
+
+	var load = current.data('load');
+	var load_type = current.data('load-type');
+	current.attr(load_type, load);
+
+	current.on('load', function() {
+		if(current_index < obj.length)
+			load_next();
+	});
+}
